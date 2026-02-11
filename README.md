cc16fda119565b19272b684a2ed26dfa6273d493
JohannaHannahoJ <joha2102@student.miun.se>
2026-02-11 11:32:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Frontendbaserad utveckling (DT211G)
+Här publicerar jag laborationer för kursen "Frontendbaserad utveckling" som är en del i webbutvecklingsprogrammet vid Mittuniversitetet.
+
+**Länk till publicerad webbplats:  **
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                sass: resolve(__dirname, "sass.html"),
+            }
+        }
+    },
+    css: {
+        devSourcemap: true
+    },
+    plugins: [
+        ViteImageOptimizer({
+            jpeg: {
+                quality: 75
+            },
+            jpg: {
+                quality: 75
+            },
+            webp: {
+                quality: 70
+            }
+        }),
+    ]
+});
