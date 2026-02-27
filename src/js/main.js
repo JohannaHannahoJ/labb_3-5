a89cece4c30972959c258fae2fc9658693ada466
JohannaHannahoJ <joha2102@student.miun.se>
2026-02-27 20:39:29 +0100

    Kommentera js-fil med jsdocs

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,42 @@
+
+/**
+ * Hambugermeny, knapp
+ */
 const hamburger = document.getElementById("hamburger");
+
+/**
+ * Navigationsmeny
+ */
 const nav = document.querySelector("nav");
 
-hamburger.addEventListener("click", () => {
-  nav.classList.toggle("active");
-  hamburger.classList.toggle("active");
-  const expanded = hamburger.classList.contains("active");
-  hamburger.setAttribute("aria-expanded", expanded);
-});
+if (hamburger && nav) {
+  /**
+   * Lyssnar efter klick på hamburgermeny-knapp
+   * 
+   */
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    hamburger.classList.toggle("active");
+    const expanded = hamburger.classList.contains("active");
+    hamburger.setAttribute("aria-expanded", expanded);
+  });
+}
 
+/**
+ * Spelknapp
+ */
 const spelKnapp = document.querySelector(".spel-knapp");
+
+/**
+ * Meddelandefält
+ */
 const resultat = document.querySelector(".resultat");
 
-spelKnapp.addEventListener("click", () => {
-  resultat.textContent = "Du klarade det. Bra gjort!";
-});
+if (spelKnapp && resultat) {
+  /**
+   * Lyssna efter klick och lämna meddelande 
+   */
+  spelKnapp.addEventListener("click", () => {
+    resultat.textContent = "Du klarade det. Bra gjort!";
+  });
+}
